df8dadd09928448e76d230c0002d23d74583eee9
Павел Богословский <91212538+PavelBogoslovskiy@users.noreply.github.com>
2022-02-01 13:40:52 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,50 +2,40 @@ import time
 from aiogram import Bot, Dispatcher, executor, types
 from main import text_gen
 from data import API_TOKEN
-
-
-
-# Configure logging # Включаем логирование
 import logging
-logging.basicConfig(level=logging.INFO)
 
+logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+CONTENT_TYPES = ["audio", "document", "photo", "sticker", "video", "video_note", "voice", "location", "contact",
+                 "new_chat_members", "left_chat_member", "new_chat_title", "new_chat_photo", "delete_chat_photo",
+                 "group_chat_created", "supergroup_chat_created", "channel_chat_created", "migrate_to_chat_id",
+                 "migrate_from_chat_id", "pinned_message"]
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def start(message: types.Message):  # много пользователей могут ему писать обработает все запросы
     user_first_name = message.from_user.first_name
     user_id = message.from_user.id
     logging.info(f'{user_id} {user_first_name} started the MusicBot at {time.asctime()}')
-    await message.reply(f"Hi, {user_first_name} !\nI'm TimeByClockBot!\nSend me a picture of the watch")
+    await message.reply(f"Привет, {user_first_name} !\nЯ MusicBot!\nПришли мне начало песни, а я ее продолжу..")
+    await message.answer_sticker(r'CAACAgIAAxkBAAEDyDxh-P_ur__U8dK0EHllcRoa0NDZAQAC6CQAAp7OCwABx40TskPHi3MjBA')
 
 
-# @dp.message_handler(content_types=['photo'])
-# async def handleR_photo(message):
-#     # media_group_id is None means single photo at message
-#     if message.media_group_id is None:
-#         user_id = message.from_user.id
-#         message_id = message.message_id
-#         img_path = 'uploaded_images/%s_%s_%s.jpg' % (user_id, 'first_name', message_id)
-#         await message.photo[-1].download(img_path)
-#         await message.answer_sticker(r'CAACAgIAAxkBAAEDwgdh8W1GvrfQg63xacgdWjJ1vtMdNwACESUAAp7OCwABwHeW06BpLm0jBA')
-#         pred = predictor(img_path)
-#         if not pred[1]:
-#             await message.answer_sticker(r'CAACAgIAAxkBAAEDwglh8W6BMY6iD-fGaUrsg6TbPnmvKgACFSUAAp7OCwAB8UnI6IhTMgABIwQ')
-#         await message.reply(pred[0])
-#
-#     else:
-#         await message.reply("Send me just one photo, please")
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
+@dp.message_handler(content_types=["text"])
+async def musgen(message: types.Message):
+    await message.answer_sticker(r'CAACAgIAAxkBAAEDyDhh-P62yzJW7H1CKGiU7e8CDFCYlQACBCUAAp7OCwABE30V36lyBr8jBA')
+    text_gen(message.text)
     await message.answer(text_gen(message.text))
-    await bot.send_audio(message.from_user.id, open("mixed_sounds.mp3" ,'rb'), performer="Нейроштука", title="Песня")
-    # await message.reply("Send me image, please")
+    await bot.send_audio(message.from_user.id, open("mixed_sounds.mp3", 'rb'), performer="Нейроштука", title="Песня")
+
+
+@dp.message_handler(content_types=CONTENT_TYPES)
+async def nottext(message: types.Message):
+    await message.answer_sticker(r'CAACAgIAAxkBAAEDyDph-P8ovipaq8MUXzlClT-zsoG_zwACCyUAAp7OCwABrghZgrziDy4jBA')
+    await message.answer('Пришли мне текст, пожалуйста!')
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import torch
-import transformers
 from transformers import GPT2Tokenizer
 import pyttsx3
 from pydub import AudioSegment
@@ -11,10 +10,14 @@ else:
 
 tokenizer = GPT2Tokenizer.from_pretrained('sberbank-ai/rugpt3small_based_on_gpt2')
 
-model = torch.load('krov_test_8.pt')
+model = torch.load('model.pt')
 
 
 def text_gen(prompt):
+    """
+    :param prompt: input text
+    :return: text
+    """
     prompt = tokenizer.encode(prompt, return_tensors='pt').to(device)
     out = model.generate(
         input_ids=prompt,
@@ -28,25 +31,20 @@ def text_gen(prompt):
         num_return_sequences=1,
     ).cpu().numpy()
     text = tokenizer.decode(out[0])
-    #     print(textwrap.fill(text, 120), end='\n------------------\n')
+
+    # Voice acting in standard Windows voice
     engine = pyttsx3.init()
-    engine.setProperty('rate', 150)  # скорость речи
+    engine.setProperty('rate', 150)
     engine.setProperty('volume', 100.0)
     voices = engine.getProperty('voices')
-    # index = 0
-    # for voice in voices:
-    #     print(f'index-> {index} -- {voice.name}')
-    #     index += 1
     engine.setProperty('voice', voices[0].id)
     engine.save_to_file(text, 'file_name.wav')
     engine.runAndWait()
-    #
-    # AudioSegment.ffmpeg = "C:\\FFmpeg\\bin\\ffmpeg.exe"
+
+    # Putting text on music
     sound1 = AudioSegment.from_mp3('songs\\bio.mp3')
     sound2 = AudioSegment.from_wav("file_name.wav")
-    # mix sound2 with sound1, starting at 5000ms into sound1)
     output = sound1.overlay(sound2, position=5000)
-    # save the result
     output.export("mixed_sounds.mp3", format="mp3")
 
     return text
